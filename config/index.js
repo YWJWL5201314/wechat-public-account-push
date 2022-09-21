3b0808b6351ac26e58dce747fe23c812b21bd64e
YWJWL5201314 <114046911+YWJWL5201314@users.noreply.github.com>
2022-09-21 12:51:50 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -88,10 +88,10 @@ export const config = {
       customizedDateList: [
         // 在一起的日子
         {"keyword": "love_day", date: "2021-01-31"},
-        // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
-        // 退伍日
-        {"keyword": "ex_day", date: "2022-09-10"},
+        
+        
+        
+        
       ]
     },
     {
